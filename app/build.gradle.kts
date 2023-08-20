ddfc4190dc8f315a811661701317bcb256ca565d
Leonardo Paz <leonardocovelodapaz@gmail.com>
2023-08-20 20:38:52 -0300

    chore: add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Navigation
+    val navVersion = "2.7.0"
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
+
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
